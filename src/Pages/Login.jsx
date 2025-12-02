9309bb66431cd23dac6c154dbd6c92f377708671
a2adib <a3dib.life@gmail.com>
2025-12-03 03:11:01 +0600

    updating Login Page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../Provider/AuthProvider';
+import toast from 'react-hot-toast';
 
 const Login = () => {
+    const { loginWithEmailPassword, handleGoogleSignin } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || "/";
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        loginWithEmailPassword(email, password)
+            .then(res => {
+                toast.success("Logged in successfully");
+                navigate(from, { replace: true });
+            })
+            .catch(err => toast.error(err.message));
+    }
+
+    const handleGoogleSignIn = () => {
+        handleGoogleSignin()
+            .then(res => {
+                toast.success("Logged in successfully");
+                navigate(from, { replace: true });
+            })
+            .catch(err => toast.error(err.message));
+    }
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now</h1>
-                    
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form className="card-body">
+                    <form onSubmit={handleLogin} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="email" className="input input-bordered" />
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered" />
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" />
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
@@ -32,7 +63,7 @@ const Login = () => {
                     <div className="card-body">
                         <p>Dont have an account? <Link to="/register" className="link link-primary">Register</Link></p>
                         <div className="divider">OR</div>
-                        <button className="btn btn-outline btn-primary">Login with Google</button>
+                        <button onClick={handleGoogleSignIn} className="btn btn-outline btn-primary">Login with Google</button>
                     </div>
                 </div>
             </div>
