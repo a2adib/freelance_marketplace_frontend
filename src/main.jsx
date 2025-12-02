722de2f722a29bdc06eea007a3f0a563edfa223d
a2adib <a3dib.life@gmail.com>
2025-12-03 02:47:19 +0600

    fixing Routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider } from 'react-router'
-import router from './router/Routes.jsx'
-import AuthProvider from './Provider/AuthProvider.jsx'
+import { RouterProvider } from 'react-router-dom'
+import router from './router/Routes'
+import AuthProvider from './Provider/AuthProvider'
+import { Toaster } from 'react-hot-toast'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />,
+      <RouterProvider router={router} />
+      <Toaster />
     </AuthProvider>
-  </StrictMode>,
+  </React.StrictMode>,
 )
--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -1,46 +1,61 @@
-import { createBrowserRouter } from "react-router";
-import Root from "../Root/Root";
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../component/MainLayout";
 import Home from "../Pages/Home";
-import Services from "../Pages/Services";
-import MyProfile from "../Pages/MyProfile";
+import AllJobs from "../Pages/AllJobs";
+import AddJob from "../Pages/AddJob";
+import MyAcceptedTasks from "../Pages/MyAcceptedTasks";
 import Login from "../Pages/Login";
 import Register from "../Pages/Register";
-import PrivetRoute from "./PrivetRoute";
+import NotFound from "../Pages/NotFound";
 import ServiceDetails from "../Pages/ServiceDetails";
+import MyProfile from "../Pages/MyProfile";
+import PrivetRoute from "./PrivetRoute";
 import ForgetPass from "../Pages/ForgetPass";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: Root,
-    children:[
-        {
-            index: true,
-            Component: Home
-        },{
-          path: "/services",
-          Component: Services
-        },{
-          path: "/myprofile",
-          element: <PrivetRoute><MyProfile /></PrivetRoute> 
-        },{
-          path: "/login",
-          Component: Login
-        },
-        {
-          path: "/signup",
-          Component: Register
-        },
-        {
-          path: "/details/:id",
-          element: <PrivetRoute><ServiceDetails/></PrivetRoute>
-        },
-        {
-          path: "/forget/:email",
-          Component: ForgetPass
-        }
-    ]
-  },
+    {
+        path: "/",
+        element: <MainLayout />,
+        errorElement: <NotFound />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/allJobs",
+                element: <AllJobs />,
+            },
+            {
+                path: "/addJob",
+                element: <AddJob />,
+            },
+            {
+                path: "/my-accepted-tasks",
+                element: <MyAcceptedTasks />,
+            },
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/register",
+                element: <Register />,
+            },
+            {
+                path: "/service/:id",
+                element: <ServiceDetails />,
+            },
+            {
+                path: "/my-profile",
+                element: <PrivetRoute><MyProfile /></PrivetRoute>,
+            },
+            {
+                path: "/forget-password",
+                element: <ForgetPass />,
+            }
+        ]
+    }
 ]);
 
-export default router
+export default router;
